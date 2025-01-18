c4d59654671a46a504e9bbe417e3a5e119f925bd
Gourab Talukdar <iamgourabtalukdar@gmail.com>
2025-01-18 11:06:00 +0530

    Day 7 Done

---
+++ day7_String/script.js
@@ -0,0 +1,52 @@
+console.dir(String);
+
+// console.log("Arpi Saha");
+// console.log(typeof "Arpi Saha");
+
+// we can apply all the methods of "String" Class in "string" data type
+let myName = "My Name is Arpi Saha. Currently I am Pursuing B.Tech";
+
+// console.log(myName.length);
+// console.log(myName.toLowerCase());
+// console.log(myName.toUpperCase());
+// console.log(myName.slice(0, 5));
+// console.log(myName.split(" "));
+// console.log(myName.split("."));
+// console.log(myName.split(". "));
+// console.log(myName.charAt(1));
+// console.log(myName.charCodeAt(11));
+
+// console.log(myName + " Hello guys");
+// console.log(myName.concat(" Hello guys"));
+// console.log(myName.concat(" Hello guys", " Hi there"));
+
+// console.log(myName.indexOf("a"));
+// console.log(myName.indexOf("a", 5));
+
+// console.log(myName.lastIndexOf("a"));
+// console.log(myName.lastIndexOf("a", 33));
+
+// console.log(myName.includes("A"));
+// console.log(myName.includes("Arpi"));
+// console.log(myName.includes("Arpi Saha"));
+
+// let numb = 5;
+// let text = numb.toString();
+// let padded = text.padStart(4, "0");
+
+// console.log(myName.padStart(80, "Hi, "));
+// console.log(myName.padEnd(80, "Hi, "));
+
+// myName =
+//   "                                 My Name is Arpi Saha. Currently I am Pursuing B.Tech                 ";
+// console.log(myName);
+// console.log(myName.trim());
+// console.log(myName.trimStart());
+// console.log(myName.trimEnd());
+
+// console.log(myName.substring(0, 5));
+
+// console.log(myName.replace("Arpi", "ABC"));
+// console.log(myName.replace("a", "x"));
+
+// console.log(myName.replaceAll("a", "x"));
