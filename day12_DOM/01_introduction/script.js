acbf1a5c14054aa3b2086b5d8f71035961b237ba
Gourab Talukdar <146658407+iamgourabtalukdar@users.noreply.github.com>
2025-03-21 19:17:49 +0530

    DOM day 1

---
+++ day12_DOM/01_introduction/script.js
@@ -0,0 +1,8 @@
+// // console.dir(document.body.style);
+// console.dir(document.body.style.margin);
+
+// // document.body.style.backgroundColor = "red";
+
+function changeColor(color) {
+  document.body.style.backgroundColor = color;
+}
